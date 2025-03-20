75f3ddbde4a4e20e3304ec05a01e352282e89c1f
Nishant Jain <89480234+Nishantjain10@users.noreply.github.com>
2025-03-20 18:48:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-# Ensure the environment variables are set
 if [ -z "$APPWRITE_PROJECT_ID" ]; then
   echo "Error: APPWRITE_PROJECT_ID is not set."
   exit 1
 fi
 
-# Replace placeholders in environment.json with actual values
 sed -i "s|\$APPWRITE_PROJECT_ID|$APPWRITE_PROJECT_ID|g" assets/environment_values/environment.json
 
 # Build Flutter Web App with environment variables
